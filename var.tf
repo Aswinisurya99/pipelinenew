311396dfd4315da27cc38f9043f24972314cda8f
Aswinisurya99 <87408864+Aswinisurya99@users.noreply.github.com>
2022-01-21 21:46:51 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,21 +1,21 @@
 variable "ec2_instance_type" {
         type=map
         default={
+                default="t2.micro"
                 new1="t2.micro"
-                new="t2.micro"
         }
 }
 variable "ec2_ami" {
         type=map
         default={
-                new1="ami-08e4e35cccc6189f4"
-                new="ami-0d43d465e2051057f"
+                default="ami-08e4e35cccc6189f4"
+                new1="ami-0d43d465e2051057f"
         }
 }
 variable "instance_name" {
         type=map
         default={
-                new1="linux"
-                new="windows"
+                default="linux"
+                new1="windows"
         }
 }
