575ecbeaee43c1cf5d20ab29abca5c98121d6854
Aswinisurya99 <87408864+Aswinisurya99@users.noreply.github.com>
2022-01-21 20:59:47 +0530

    Update linux.tf

--- linux.tf
+++ linux.tf
@@ -1,13 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+        region="us-east-1"
 }
-resource "aws_instance" "ec2new" {
-   ami           = "ami-08e4e35cccc6189f4" 
-   instance_type =  "t2.micro"
-   count = 1
-  tags = {
-         Name = "linux"
-  }
+resource "aws_instance" "Workspace-vm" {
+        ami=lookup(var.ec2_ami,terraform.workspace)
+        instance_type=lookup(var.ec2_instance_type,terraform.workspace)
+        tags={
+                Name=lookup(var.instance_name,terraform.workspace)
+        }
 }
-
-
