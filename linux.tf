9af0bcbc61363ead924a898cd35f02f01416c110
Aswinisurya99 <87408864+Aswinisurya99@users.noreply.github.com>
2022-01-21 20:32:16 +0530

    Update linux.tf

--- linux.tf
+++ linux.tf
@@ -2,15 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "ec2new" {
-
    ami           = "ami-08e4e35cccc6189f4" 
    instance_type =  "t2.micro"
    count = 1
-
-   tags = {
-           Name = "linux"
-   }
-
+  tags = {
+         Name = "linux"
+  }
 }
 
 
